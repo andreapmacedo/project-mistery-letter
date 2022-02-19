406117445041c9bc60e0da0751ac08c1e89c8902
Andre <aa.andremacedo@gmail.com>
2022-02-19 14:59:18 -0300

    adiciona a solução para o desafio 3

--- script.js
+++ script.js
@@ -3,31 +3,38 @@ const inputTextField = document.getElementById('carta-texto');
 const generatedLetter = document.getElementById('carta-gerada');
 
 
-
+//- Logica para verificação dos requisitos e montagem da carta.
 function generateLetter() {
-
+  
+  //-- Remover as tags `span` caso existam.
+  while (generatedLetter.firstChild) {
+    generatedLetter.removeChild(generatedLetter.firstChild);
+  }
+  //-- quebra a frase em palavras e adiciona em uma lista.
   let sentence = (inputTextField.value).split(' ');
   
-  //console.log(sentence);
-  // O código abaixo foi pesquisado em:
-  // https://pt.stackoverflow.com/questions/484146/como-contar-ocorr%C3%AAncias-de-um-valor-dentro-de-um-array
+  // O código abaixo foi pesquisado em: https://pt.stackoverflow.com/questions/484146/como-contar-ocorr%C3%AAncias-de-um-valor-dentro-de-um-array
+  //-- conta a quantidade de `spaces bars` para verificar se a sentença é formada só por este tipo de caracter
   let countSpaceBar = sentence.filter(x => x == "").length;
 
-  
+  //-- Caso a sentença seja válida, cria os elementos `span` que construirão a carta.
   if (sentence.length > 0 && countSpaceBar < sentence.length) {
-    for (let i in sentence) {
-      if (sentence[i] !== ''){
-        const node = document.createElement('span');
-        node.innerText = sentence[i];
-        generatedLetter.appendChild(node);
-      }
+    for (let i in sentence)
+    {
+      createSpan(sentence[i]);
     }
   } else {
-    const node = document.createElement('span');
-    node.innerText = 'Por favor, digite o conteúdo da carta.';
-    generatedLetter.appendChild(node);
+    createSpan('Por favor, digite o conteúdo da carta.');
   }
   
+}
+
+//-- Adiciona os elementos `span` ao seu pai `p`
+function createSpan(phrase){
+  const node = document.createElement('span');
+  node.innerText = phrase;
+  // node.innerHTML = phrase;
+  generatedLetter.appendChild(node);
 }
 
 
