7943d1bfd55fa75455251493e34254a2f2c939cf
Andre <aa.andremacedo@gmail.com>
2022-02-19 15:14:59 -0300

    adiciona a solução para os desafios que vão do 9 ao 16

--- script.js
+++ script.js
@@ -1,22 +1,17 @@
 const btnGenerateLetter = document.getElementById('criar-carta');
 const inputTextField = document.getElementById('carta-texto');
 const generatedLetter = document.getElementById('carta-gerada');
-
-
 //- Logica para verificação dos requisitos e montagem da carta.
 function generateLetter() {
-  
   //-- Remover as tags `span` caso existam.
   while (generatedLetter.firstChild) {
     generatedLetter.removeChild(generatedLetter.firstChild);
   }
   //-- quebra a frase em palavras e adiciona em uma lista.
   let sentence = (inputTextField.value).split(' ');
-  
   // O código abaixo foi pesquisado em: https://pt.stackoverflow.com/questions/484146/como-contar-ocorr%C3%AAncias-de-um-valor-dentro-de-um-array
   //-- conta a quantidade de `spaces bars` para verificar se a sentença é formada só por este tipo de caracter
   let countSpaceBar = sentence.filter(x => x == "").length;
-
   //-- Caso a sentença seja válida, cria os elementos `span` que construirão a carta.
   if (sentence.length > 0 && countSpaceBar < sentence.length) {
     for (let i in sentence)
@@ -28,7 +23,6 @@ function generateLetter() {
   }
   
 }
-
 //-- Adiciona os elementos `span` ao seu pai `p`
 function createSpan(phrase){
   const node = document.createElement('span');
@@ -36,10 +30,8 @@ function createSpan(phrase){
   // node.innerHTML = phrase;
   generatedLetter.appendChild(node);
 }
-
-
+//-- Coloca as funções de evento `em escuta` ao carregar a página no nagegador
 window.onload = function () {
-  
   btnGenerateLetter.addEventListener('click', generateLetter);
 
 }
